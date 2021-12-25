8dad75a39cae539d2291def4822298d295fb1190
Kosuke Toyama <80808445+tosan777@users.noreply.github.com>
2021-12-25 22:56:44 +0900

    TODOリスト全てののボタンの機能

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 "use strict";
 
+/* add-button押下後イベント */
 const onClickAdd = () => {
   /* add-textの値を取得 */
   const inputText = document.getElementById("add-text").value;
+  if (inputText === "") {
+    return "";
+  }
   /* add-textの値を空にして次入力しやすいようにする */
   document.getElementById("add-text").value = "";
 
@@ -20,11 +24,32 @@ const onClickAdd = () => {
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   completeButton.className = "button";
+  /* 完了ボタン押下後のイベント */
+  completeButton.addEventListener("click", () => {
+    const backButton = document.createElement("button");
+    backButton.className = "button";
+    backButton.innerText = "戻す";
+    li.removeChild(completeButton);
+    li.removeChild(deleteButton);
+    li.appendChild(backButton);
+    document.getElementById("complete-list").appendChild(li);
+
+    /* 戻るボタンの作成 */
+    backButton.addEventListener("click", () => {
+      li.appendChild(completeButton);
+      li.appendChild(deleteButton);
+      li.removeChild(backButton);
+      ul.appendChild(li);
+    });
+  });
 
   /* 削除ボタンの生成 */
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.className = "button";
+  deleteButton.addEventListener("click", () => {
+    li.remove();
+  });
 
   /* list-rowの子要素に各要素を設定 */
   li.appendChild(p);
@@ -34,7 +59,6 @@ const onClickAdd = () => {
   /* incomplete-listに未完了のリストを追加 */
   const ul = document.getElementById("incomplete-list");
   ul.appendChild(li);
-  console.log(ul);
 };
 /* add-buttonをクリックした時のイベント */
 document.getElementById("add-button").addEventListener("click", onClickAdd);
